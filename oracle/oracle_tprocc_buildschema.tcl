b106593e02d76bbcb6b52735f622e3e1c4546750
MaximilianJahns <jahns@F2UI-NBP203.f2.htw-berlin.de>
2026-05-11 12:45:44 +0200

    Add Oracle Free Edition TPC-C benchmark support

---
+++ oracle/oracle_tprocc_buildschema.tcl
@@ -0,0 +1,26 @@
+#!/bin/tclsh
+puts "SETTING CONFIGURATION"
+dbset db ora
+dbset bm TPC-C
+diset connection system_user system
+diset connection system_password manager
+diset connection instance //oracle_container:1521/freepdb1
+set vu [ numberOfCPUs ]
+set warehouse [ expr {$vu * 5} ]
+diset tpcc count_ware $warehouse
+diset tpcc num_vu $vu
+diset tpcc tpcc_user tpcc
+diset tpcc tpcc_pass tpcc
+diset tpcc tpcc_def_tab users
+diset tpcc tpcc_def_temp temp
+if { $warehouse >= 200 } {
+    diset tpcc partition true
+    diset tpcc hash_clusters true
+    diset tpcc tpcc_ol_tab users
+} else {
+    diset tpcc partition false
+    diset tpcc hash_clusters false
+}
+puts "SCHEMA BUILD STARTED"
+buildschema
+puts "SCHEMA BUILD COMPLETED"
